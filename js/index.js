876971d8f724fd9756c799bb80313ade9077f3ab
Rahul Dabgotra <rahuldabgo@gmail.com>
2025-04-23 02:42:52 -0400

    changes for static site

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
-    if (!sessionStorage.getItem('loggedIn')) {
-        window.location.href = 'login.html';
-    }
-});
+//     if (!sessionStorage.getItem('loggedIn')) {
+//         window.location.href = 'login.html';
+//     }
+// });
 
-document.getElementById('logoutLink').addEventListener('click', function(e) {
-    e.preventDefault();
-    sessionStorage.removeItem('loggedIn');
-    window.location.href = 'login.html';
-});
+// document.getElementById('logoutLink').addEventListener('click', function(e) {
+//     e.preventDefault();
+//     sessionStorage.removeItem('loggedIn');
+//     window.location.href = 'login.html';
+// });
